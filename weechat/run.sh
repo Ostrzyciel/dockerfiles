4825b8848b2d0992944a5f4675a0d0398384de5d
Ostrzyciel <ostrzycielnozyczek@gmail.com>
2021-07-05 21:50:22 +0200

    Update weechat dockerfile

--- weechat/run.sh
+++ weechat/run.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
-# usermod -o -u "$UID" weechat
-# groupmod -o -g "$GID" weechat
-
-# chown -R weechat:weechat /weechat
-
+chown -R weechat:weechat /weechat
 exec su-exec weechat /usr/bin/weechat "$@"
